b5ab86545aff5cb7fc6dcbca095ef7b98fe0d585
Igor M <iggorm@yahoo.com.br>
2021-03-24 15:35:35 -0300

    feat: implementando componente inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { environment } from './../../environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    // if(environment.token ==''){
+    //   alert('Sua sessão inspirou. Faça o login novamente!')
+    //   this.router.navigate(['/entrar'])
+    // }
   }
 
 }
