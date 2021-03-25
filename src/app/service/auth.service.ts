3e2ccb290710558a4fc0e2c71b6d3848b44065c0
Igor M <iggorm@yahoo.com.br>
2021-03-25 16:21:55 -0300

    feat: implementação do get e post de tema

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -12,14 +12,13 @@ export class AuthService {
 
   constructor( private http: HttpClient ) { }
 
-  baseUrl = environment.server + environment.port
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>(`${this.baseUrl}/usuarios/logar`, userLogin)
+    return this.http.post<UserLogin>(`${environment.server}${environment.port}/usuarios/logar`, userLogin)
   }
 
   cadastrar( user: User ): Observable<User>{
-    return this.http.post<User>(`${this.baseUrl}/usuarios/cadastrar`, user)
+    return this.http.post<User>(`${environment.server}${environment.port}/usuarios/cadastrar`, user)
   }
 
   logado(){
---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,29 @@
+import { environment } from 'src/environments/environment.prod';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`${environment.server}${environment.port}/tema`, this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    // return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
+    return this.http.post<Tema>(`${environment.server}${environment.port}/tema`, tema, this.token)
+  }
+
+}
+
